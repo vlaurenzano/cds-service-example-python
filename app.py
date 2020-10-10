808c653c4abd7c4c01d689c9415724c7434b5655
vlaurenzano <vincent.laurenzano@gmail.com>
2020-10-10 18:04:40 -0400

    a

--- app.py
+++ app.py
@@ -5,11 +5,9 @@ app = cds.App()
 
 
 @app.patient_view("patient-greeting", "The patient greeting service greets a patient!", title="Patient Greeter")
-def greeting(r: cds.PatientViewRequest) -> cds.Response:
-    resp = cds.Response()
-    resp.cards = [cds.Card.info("hello world!", "demo_service")]
-    resp.httpStatusCode = 200
-    return resp
+def greeting(r: cds.PatientViewRequest, response: cds.Response) -> cds.Response:
+    response.cards = [cds.Card.info("hello world!", "demo_service")]
+    response.httpStatusCode = 200
 
 
 if __name__ == '__main__':

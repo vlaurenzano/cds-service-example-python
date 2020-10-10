b5ae5f86c9ddfc0cdfa58168bd00d8c315282bb5
vlaurenzano <vincent.laurenzano@gmail.com>
2020-10-10 18:12:22 -0400

    change id

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 app = cds.App()
 
 
-@app.patient_view("patient-greeting", "The patient greeting service greets a patient!", title="Patient Greeter")
+@app.patient_view("hello-world", "The patient greeting service greets a patient!", title="Patient Greeter")
 def greeting(r: cds.PatientViewRequest, response: cds.Response) -> cds.Response:
     response.cards = [cds.Card.info("hello world!", "demo_service")]
     response.httpStatusCode = 200
